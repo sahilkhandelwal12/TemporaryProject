54ffe49b3af3207ccd30f4d7e4a4a4ef0ed964a2
admin <>
2022-08-04 17:50:46 +0000

    {/src/main/java/com/dummyspaces/temporaryproject/temp.java}

--- src/main/java/com/dummyspaces/temporaryproject/temp.java
+++ src/main/java/com/dummyspaces/temporaryproject/temp.java
@@ -6,12 +6,23 @@ package com.dummyspaces.temporaryproject;
 
 public class temp implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public temp() {
-    }
+	private java.lang.String name;
 
+	public temp() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public temp(java.lang.String name) {
+		this.name = name;
+	}
 
 }
